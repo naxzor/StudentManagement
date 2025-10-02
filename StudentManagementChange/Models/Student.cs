c338a66ece6b7101da7c2c347183f6feb63262e3
NaX <naxzor1989@hotmail.com>
2025-10-03 01:08:16 +0200

    add nullable middle name to student

--- StudentManagementChange/Models/Student.cs
+++ StudentManagementChange/Models/Student.cs
@@ -4,6 +4,7 @@ public class Student
 {
     public int Id { get; set; }
     public string FirstName { get; set; } = null!;
+    public string? MiddleName { get; set; } 
     public string LastName  { get; set; } = null!;
     public string Email     { get; set; } = null!;
     public DateTime EnrollmentDate { get; set; }
