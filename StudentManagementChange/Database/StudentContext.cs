69389953c92bed49d8e624f25997bea3039a020d
NaX <naxzor1989@hotmail.com>
2025-10-03 00:11:12 +0200

    add dbcontext with modebuilder

---
+++ StudentManagementChange/Database/StudentContext.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagementChange.Models;
+
+namespace StudentManagementChange.Data;
+
+public class StudentContext : DbContext
+{
+    public StudentContext(DbContextOptions<StudentContext> options) : base(options) {}
+
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+
+    protected override void OnModelCreating(ModelBuilder b)
+    {
+        b.Entity<Student>()
+            .HasIndex(s => s.Email)
+            .IsUnique();
+
+
+        b.Entity<Enrollment>()
+            .HasOne(e => e.Student)
+            .WithMany(s => s.Enrollments)
+            .HasForeignKey(e => e.StudentId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        b.Entity<Enrollment>()
+            .HasOne(e => e.Course)
+            .WithMany()
+            .HasForeignKey(e => e.CourseId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        b.Entity<Enrollment>()
+            .Property(e => e.Grade)
+            .HasPrecision(3, 1);
+    }
+}
