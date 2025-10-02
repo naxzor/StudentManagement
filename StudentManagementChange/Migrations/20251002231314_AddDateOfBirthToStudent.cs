b095ff3e796772780a724d3e1417bc2fe5ed4151
NaX <naxzor1989@hotmail.com>
2025-10-03 01:14:34 +0200

    add date of birth to student

---
+++ StudentManagementChange/Migrations/20251002231314_AddDateOfBirthToStudent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagementChange.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateOfBirthToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Students",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Students");
+        }
+    }
+}
--- StudentManagementChange/Models/Student.cs
+++ StudentManagementChange/Models/Student.cs
@@ -7,6 +7,7 @@ public class Student
     public string? MiddleName { get; set; } 
     public string LastName  { get; set; } = null!;
     public string Email     { get; set; } = null!;
+    public DateOnly? DateOfBirth { get; set; }
     public DateTime EnrollmentDate { get; set; }
 
     public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
