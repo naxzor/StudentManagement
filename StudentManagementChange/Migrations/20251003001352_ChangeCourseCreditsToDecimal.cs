51d13b6a2dc044d7f8f82180c3b0821c10f25b06
NaX <naxzor1989@hotmail.com>
2025-10-03 02:17:05 +0200

    changing Credit to decimal

---
+++ StudentManagementChange/Migrations/20251003001352_ChangeCourseCreditsToDecimal.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagementChange.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCourseCreditsToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Credits",
+                table: "Courses",
+                type: "numeric(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Credits",
+                table: "Courses",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+        }
+    }
+}
--- StudentManagementChange/Models/Course.cs
+++ StudentManagementChange/Models/Course.cs
@@ -4,7 +4,7 @@ public class Course
 {
     public int Id { get; set; }
     public string Title { get; set; } = null!;
-    public int Credits  { get; set; }
+    public decimal Credits  { get; set; }
     public int? InstructorId { get; set; }
     public Instructor? Instructor { get; set; }
     public int DepartmentId { get; set; }
